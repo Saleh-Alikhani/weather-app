5a859061a8e366664113204352ed76329a0dab51
Saleh Alikhani <salehalikhani@gmail.com>
2024-04-04 16:45:23 +0330

    add loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
+import loading from '@assets/lotties/Animation - 1712234874068.json';
 import Page from '@components/Page';
+import { Row } from 'antd';
 import Head from 'next/head';
+import { useState } from 'react';
+import Lottie from 'react-lottie';
+import styled from 'styled-components';
+
+const StyledLottieWrapper = styled(Row)`
+  background-color: #f2f2f2;
+`;
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 4000);
+
   return (
     <>
       <Head>
@@ -11,7 +26,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Page />
+        {isLoading ? (
+          <StyledLottieWrapper>
+            <Lottie
+              style={{ height: '100vh' }}
+              options={{ animationData: loading }}
+            />
+          </StyledLottieWrapper>
+        ) : (
+          <Page />
+        )}
       </main>
     </>
   );
